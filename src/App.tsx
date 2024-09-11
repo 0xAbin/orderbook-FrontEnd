9b3787d51ceb531d015182898902f6849c5d083d
0xabin <abinbvinod@gmail.com>
2024-09-11 17:20:57 +0530

    FrontEnd Init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center pt-7 font-bold text-2xl">ORDER BOOK</div>
+    </>
+  )
+}
+
+export default App
